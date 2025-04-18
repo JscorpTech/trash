a2361248edd015d0928769d20508d6877cbc381e
JAvohir <uchihajavohir0609@gmail.com>
2025-04-18 19:10:17 +0500

    first commit

--- core/app/models/company.py
+++ core/app/models/company.py
@@ -20,8 +20,8 @@ class CompanyModel(AbstractBaseModel):
     staff = PositiveIntegerField(_("Number of employees"))
     work = PositiveIntegerField(_("Hours of work "))
     countries = PositiveIntegerField(_("Countries"))
-    about_us_title = CharField(_("About us title"), max_length=255)
-    about_us_description = TextField(_("About us description"))
+    about_us_title = CharField(_("About us title"), max_length=255, default="")
+    about_us_description = TextField(_("About us description"), default="")
 
     def __str__(self):
         return f"{self.email} - {self.phone}"
