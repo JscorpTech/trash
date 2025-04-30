7807f1abad9c560e745822378f9d8c926afa40fa
A'zamov Samandar <admin@jscorp.uz>
2025-04-30 13:07:40 +0500

    😊 Statik va media fayllarining URL manzillari optimallashtirildi, kod yanada toza va o'qilishi osonlashdi.

--- config/urls.py
+++ config/urls.py
@@ -39,5 +39,5 @@ urlpatterns += [
 ################################
 # Static Files
 ################################
-urlpatterns += static("backend/backend/" + settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static("backend/backend/" + settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
