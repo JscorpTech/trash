90277eeb3dc2234eddeecf56f4fa3d31031a6529
A'zamov Samandar <admin@jscorp.uz>
2025-04-30 12:19:52 +0500

    🚀 Kodga o'qilishi osonroq bo'lishi uchun tartibga solindi.

--- config/urls.py
+++ config/urls.py
@@ -33,7 +33,11 @@ urlpatterns += [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path("backend/api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path(
+        "backend/api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 ################################
