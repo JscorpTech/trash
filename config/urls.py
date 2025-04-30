6b295df94e80df542fd4aa91d1216ada73c78adf
A'zamov Samandar <admin@jscorp.uz>
2025-04-30 12:32:33 +0500

    🔧 O'zgarishlar backend yo'lini qo'shib, statik va media fayllarini yangi joylashuvda ko'rsatmoqda.

--- config/urls.py
+++ config/urls.py
@@ -43,5 +43,5 @@ urlpatterns += [
 ################################
 # Static Files
 ################################
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static("backend" + settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static("backend" + settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
